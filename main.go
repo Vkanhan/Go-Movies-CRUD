638431fbe3a45b5e3e6e57e0096f92b0b0006599
Vishnu kanhan <kanhanvishnu@gmail.com>
2024-02-23 14:38:37 +0530

    Fully corrected and tested

--- main.go
+++ main.go
@@ -91,8 +91,8 @@ func main() {
 	r.HandleFunc("/movies", getMovies).Methods("GET")
 	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
 	r.HandleFunc("/movies", createMovie).Methods("POST")
-	r.HandleFunc("/movies", updateMovie).Methods("PUT")
-	r.HandleFunc("/movies", deleteMovie).Methods("DELETE")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
 
 	fmt.Printf("Starting server at port 8000\n")
 	if err := http.ListenAndServe(":8000", r); err != nil {
